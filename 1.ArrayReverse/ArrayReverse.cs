237bc200e517d985d22e7c5868710a2f3a7c8932
Vlado <vlado_web@abv.bg>
2018-10-24 23:30:45 +0300

    2.ResetEvenIndexes

---
+++ 1.ArrayReverse/ArrayReverse.cs
@@ -0,0 +1,30 @@
+﻿namespace _1.ArrayReverse
+{
+    using System;
+    public class ArrayReverse
+    {
+        public static void Main()
+        {
+            var array = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            int arrayLenght = array.Length;
+
+            while (arrayLenght != 0)
+            {
+                for (int i = 1; i <= arrayLenght - 1; i++)
+                {
+                    var first = array[i];
+                    var second = array[i - 1];
+
+                    array[i] = second;
+                    array[i - 1] = first;
+
+                }
+
+                arrayLenght--;
+            }              
+            
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
---
+++ 2.ResetEvenIndexes/ResetEvenIndexes.cs
@@ -0,0 +1,27 @@
+﻿namespace _2.ResetEvenIndexes
+{
+    using System;
+    using System.Linq;
+    public class ResetEvenIndexes
+    {
+        public static void Main()
+        {
+
+            var array = Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n))
+                .ToArray();
+
+            int arrayLenght = array.Length;
+
+            for (int i = 0; i <=arrayLenght - 1; i++)
+            {
+                if(i%2==0)
+                array[i] = 0;
+            }
+
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
+
